44c451b9215c278f450d6d3e21024094e95d3d97
shackra <elcorreo@deshackra.com>
2014-05-24 21:25:23 -0600

    Retornando la dirección IP del usuario correctamente (los proxies son tomados en cuenta ahora)

--- myip/index/views.py
+++ myip/index/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
+    ip = request.META.get("HTTP_X_FORWARDED_FOR", None)
+    if ip:
+        # X_FORWARDED_FOR returns client1, proxy1, proxy2,...
+        ip = ip.split(", ")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR", "")
+
     return render(request, "index.html",
-                  {"remote_ip": request.META["REMOTE_ADDR"]})
+                  {"remote_ip": ip})
